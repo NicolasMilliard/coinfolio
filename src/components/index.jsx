f6cbc74a5ab373574be637884f63ec6765273467
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-07-28 20:08:11 +0200

    Add charts exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,5 @@ export { default as ThemeSettings } from './ThemeSettings';
 export { default as UserProfile } from './UserProfile';
 export { default as NavBar } from './NavBar';
 export { default as Sidebar } from './Sidebar';
+export { default as LineChart } from './charts/LineChart';
+export { default as PieChart } from './charts/PieChart';
