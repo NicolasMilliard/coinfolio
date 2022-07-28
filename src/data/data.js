5334ddec8582d1c5063b5fd66b740493d0acc0c5
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-07-28 20:08:57 +0200

    Add data for LineChart and PieChart

--- src/data/data.js
+++ src/data/data.js
@@ -76,3 +76,52 @@ export const links = [
         ],
     },
 ]
+
+export const lineChartData = [
+    [
+        { x: new Date(2022, 6, 25), y: 9864.58 },
+        { x: new Date(2022, 6, 26), y: 10221.63 },
+        { x: new Date(2022, 6, 27), y: 9951.47 },
+        { x: new Date(2022, 6, 28), y: 10123.75 },
+        { x: new Date(2022, 6, 29), y: 10861.23 },
+        { x: new Date(2022, 6, 30), y: 11654.24 },
+        { x: new Date(2022, 6, 31), y: 11898.68 },
+    ]
+];
+
+export const lineCustomSeries = [
+{ dataSource: lineChartData[0],
+    xName: 'x',
+    yName: 'y',
+    name: 'Total Capital',
+    width: '2',
+    marker: { visible: false, width: 0, height: 0 },
+    type: 'Line' },  
+];
+
+export const linePrimaryXAxis = {
+    valueType: 'DateTime',
+    labelFormat: 'd-0M',
+    intervalType: 'Days',
+    edgeLabelPlacement: 'Shift',
+    majorGridLines: { width: 0 },
+    background: 'white',
+};
+  
+export const linePrimaryYAxis = {
+    labelFormat: '${value}',
+    rangePadding: 'None',
+    minimum: 9000,
+    maximum: 12000,
+    interval: 1000,
+    lineStyle: { width: 0 },
+    majorTickLines: { width: 0 },
+    minorTickLines: { width: 0 },
+};
+
+export const pieChartData = [
+    { x: 'USDC', y: 1224, text: '1224.57 USDC' },
+    { x: 'BTC', y: 2895, text: '0.1246 BTC' },
+    { x: 'ETH', y: 2678, text: '1.6146 ETH' },
+    { x: 'BNB', y: 2124, text: '7.8695 BNB' }
+];
