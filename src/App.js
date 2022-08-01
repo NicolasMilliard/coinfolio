c71f6ab602b33197ea32889f1914b84d8b19c9e9
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-01 10:48:19 +0200

    Remove settings and light theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
-import { NavBar, Sidebar, ThemeSettings } from './components'
+import { NavBar, Sidebar } from './components'
 import { Bnb, Btc, Eth, Calendar, Notes, Overview, Portfolio, TodoList, Trending, Watchlist } from './pages'
 
 import { useStateContext } from './contexts/ContextProvider'
@@ -16,18 +15,10 @@ const App = () => {
   return (
     <div>
         <BrowserRouter>
-            <div className='flex relative dark:bg-main-dark-bg'>
-                {/* Settings */}
-                <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
-                    <TooltipComponent content='Settings' position='Top'>
-                        <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white rounded-full' style={{ background: 'blue' }}>
-                            <FiSettings />
-                        </button>
-                    </TooltipComponent>
-                </div>
+            <div className='flex relative'>
                 {/* Sidebar */}
                 {activeMenu ? (
-                    <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                    <div className='w-72 fixed sidebar bg-[#182b4b]'>
                         <Sidebar />
                     </div>
                 ) : (
@@ -36,8 +27,8 @@ const App = () => {
                     </div>
                 )}
                 {/* Navigation bar */}
-                <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
-                    <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                <div className={`bg-[#111e35] min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+                    <div className='fixed md:static bg-[#111e35] navbar w-full'>
                         <NavBar />
                     </div>
                     {/* Routes */}
