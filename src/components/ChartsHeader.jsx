eeee053f3c497017186bea7fb5c4ce4abb262750
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-01 10:48:35 +0200

    Init commit

---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ChartsHeader = ({ category, title }) => {
+  return (
+    <div className="mb-10">
+      <div>
+        <p className="text-lg text-gray-400">Chart</p>
+        <p className="text-3xl font-extrabold tracking-tight text-gray-200">{category}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ChartsHeader
--- src/pages/Btc.jsx
+++ src/pages/Btc.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, CandleSeries, Tooltip, DateTime, Zoom, Logarithmic, Crosshair } from '@syncfusion/ej2-react-charts'
+
+import { bitcoinChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../data/data'
+import { useStateContext } from '../contexts/ContextProvider'
+import { ChartsHeader } from '../components/'
+
+const date1 = new Date('2012, 4, 1');
+
+function filterValue(value) {
+  if(value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+
+const returnValue = bitcoinChartData.filter(filterValue);
 
 const Btc = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Btc</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-[#182b4b] rounded-3xl">
+      <ChartsHeader category="Bitcoin" />
+      <div className="w-full">
+        <ChartComponent
+          id="bitcoin-chart"
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
+          background={"#182b4b"}
+        >
+          <Inject services={[CandleSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="low"
+              name="Bitcoin"
+              type="Candle"
+              enableSolidCandles="true"
+              bullFillColor= "#f23645"
+              bearFillColor="#089981"
+              low="low"
+              high="high"
+              open="open"
+              close="close"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
   )
 }
 
