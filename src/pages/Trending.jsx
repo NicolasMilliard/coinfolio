b3682c8793ddc52f86c6da277c21f3585f4e74e2
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-05 15:13:13 +0200

    Init commit

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, Edit, Inject } from '@syncfusion/ej2-react-grids'
+
+import { trendingCryptoData, cryptoGrid } from '../data/data'
+
+import { Header } from '../components'
 
 const Trending = () => {
   return (
-    <div>Trending</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 rounded-3xl bg-[#182b4b]'>
+      <Header category='Page' title='Trending assets' />
+      <GridComponent
+        id='cryptogridcomp'
+        dataSource={trendingCryptoData}
+        allowPaging
+        allowSorting
+        style={{ backgroundColor: '#111e35'}}
+      >
+        <ColumnsDirective>
+          {cryptoGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Sort, Filter, Page]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, Edit, Inject } from '@syncfusion/ej2-react-grids'
+
+import { favoritesCryptoData, cryptoGrid } from '../data/data'
+
+import { Header } from '../components'
 
 const Watchlist = () => {
   return (
-    <div>Watchlist</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 rounded-3xl bg-[#182b4b]'>
+      <Header category='Page' title='Your Favorites assets' />
+      <GridComponent
+        id='watchlistgridcomp'
+        dataSource={favoritesCryptoData}
+        allowPaging
+        allowSorting
+        style={{ backgroundColor: '#111e35'}}
+      >
+        <ColumnsDirective>
+          {cryptoGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Sort, Filter, Page]} />
+      </GridComponent>
+    </div>
   )
 }
 
