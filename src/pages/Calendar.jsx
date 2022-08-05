4706add55058b7629767e4fc6b65ca153b51baf6
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-05 17:50:49 +0200

    Init commit

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { ScheduleComponent, Month, Agenda, Inject, ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule'
+
+import { scheduleData } from '../data/data'
+import { Header } from '../components'
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-[#182b4b] rounded-3xl'>
+      <Header category='Tool' title='ICO Calendar' />
+      <ScheduleComponent
+        height='650px'
+        eventSettings={{ dataSource: scheduleData }}     
+        selectedDate={new Date(2022, 7, 1)}
+        currentView='Month'
+      >
+        <ViewsDirective>
+          <ViewDirective option='Month' showWeekend={false} />
+        </ViewsDirective>
+        <Inject services={[Month, Agenda]} />
+      </ScheduleComponent>
+    </div>
   )
 }
 
