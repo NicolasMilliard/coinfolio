e87662389e1135e95d4e2e2b906e26f1207dcf74
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-08-06 18:49:27 +0200

    Init commit

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor'
+
+import { NotesData }  from '../data/data'
+import { Header } from '../components'
 
 const Notes = () => {
   return (
-    <div>Notes</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-[#182b4b] rounded-3xl'>
+      <Header category='Tools' title='Notes' />
+      <RichTextEditorComponent>
+        <NotesData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
